2b7bf3ebdd79a8f441a9af134454d1adf1bcc7f8
josetanta <jose.tanta.27@unsch.edu.pe>
2024-07-07 19:28:40 -0500

    init

---
+++ src/main/java/edu/systemia/auditing_entities/AuditingEntitiesApplication.java
@@ -0,0 +1,13 @@
+package edu.systemia.auditing_entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditingEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditingEntitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/systemia/auditing_entities/AuditingEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.systemia.auditing_entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditingEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
